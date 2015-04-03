668458b29dfc127c313dedbc73b70ba030a545af
chengdu28 <joshbray@worldsbydesign.com>
2015-04-03 16:15:50 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+var config = {}
+
+// Instance Settings
+config.instance = '@empjobrayeureka.service-now.com/incident.do?WSDL';
+config.username =  'admin';
+config.password = 'admin'
+
+// Assignment Settings
+config.assigned_state = '2';
+config.assigned_to = '5137153cc611227c000bbd1bd8cd2005';
+config.assignment_group = 'b85d44954a3623120004689b2d5dd60a';
+
+// Search Settings
+config.search_priority = '1'; 
+config.search_state = '1';
+
+// Code Settings
+config.xmlDocPath = '/Users/josh.bray/node_modules/xmldoc/lib/xmldoc.js'
+config.serialport =  '/dev/tty.usbmodem1451';
+
+
+module.exports = config;
